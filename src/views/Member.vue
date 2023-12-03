efd3823c86bb765e676b675a42bbdf148b71e703
fskhri <fakhrijuanda88@gmail.com>
2023-12-03 18:07:50 +0700

    Add Profile Member

--- src/views/Member.vue
+++ src/views/Member.vue
@@ -6,9 +6,9 @@
         <span class="red--text font-weight-black headline">Members</span>
         <div class="py-2"></div>
         <!-- Add your members content here -->
-        <v-slide-group v-model="model" class="" mandatory show-arrows="hide-md-and-up">
+        <v-slide-group shwo-arrow v-model="model" class="" mandatory show-arrows="hide-md-and-up">
           <v-slide-item v-for="member in members" :key="member.name" v-slot:default="{ active, toggle}">
-            <v-card :color="active ? 'red darken-4' : 'grey lighten-1'" class="ma-4" height="200" width="100" @click="toggle">
+            <v-card :color="active ? 'red darken-4' : 'grey lighten-1'" class="ma-4" height="520" width="520" @click="toggle">
               <v-img :src="member.src"></v-img>
               <v-row class="fill-height" align="center" justify="center">
                 <v-scale-transition>
@@ -20,11 +20,6 @@
         </v-slide-group>
         <div class="py-3"></div>
       </v-container>
-  
-      <v-layout justify-center>
-      <v-btn outlined color="red">Load More</v-btn>
-      <div class="py-3"></div>
-    </v-layout>
 
     <div class="py-5"></div>
     <v-footer color="transparent" padless>
@@ -47,33 +42,33 @@
     data() {
       return {
         members: [
-        { name: 'Adzana Shaliha', src: 'https://jkt48.com/profile/adzana_shaliha.jpg?v=20230116', link: 'jkt48.com'},
-        { name: 'Amanda Sukma', src: 'https://jkt48.com/profile/amanda_sukma.jpg?v=20230530'},
-        { name: 'Angelina Christy', src: 'https://jkt48.com/profile/angelina_christy.jpg?v=20230116'},
-        { name: 'Aurellia', src: 'https://jkt48.com/profile/aurellia.jpg?v=20230531'},
-        { name: 'Azizi Asadel', src: 'https://jkt48.com/profile/azizi_asadel.jpg?v=20230116'},
+        { name: 'Adzana Shaliha', src: 'https://pbs.twimg.com/media/GAU1N_rbIAAruLd?format=jpg&name=medium', link: 'jkt48.com'},
+        { name: 'Amanda Sukma', src: 'https://pbs.twimg.com/media/GAU30TgbQAA43x0?format=jpg&name=medium'},
+        { name: 'Angelina Christy', src: 'https://pbs.twimg.com/media/GAU6s73bwAAxGE5?format=jpg&name=medium'},
+        { name: 'Aurellia', src: 'https://pbs.twimg.com/media/GAU2GCabkAAxj2Q?format=jpg&name=medium'},
+        { name: 'Azizi Asadel', src: 'https://pbs.twimg.com/media/GAVfU3ObcAAXOGU?format=jpg&name=medium'},
         { name: 'Callista Alfia', src: 'https://jkt48.com/profile/callista_alifia.jpg?v=20230531'},
         { name: 'Cornelia Vanisa', src: 'https://jkt48.com/profile/cornelia_vanisa.jpg?v=20230116'},
         { name: 'Febriola Sinambela', src: 'https://jkt48.com/profile/febriola_sinambela.jpg?v=20230116'},
         { name: 'Feni Fitriyanti', src: 'https://jkt48.com/profile/feni_fitriyanti.jpg?v=20230116'},
-        { name: 'Fiony Alveria', src: 'https://jkt48.com/profile/fiony_alveria.jpg?v=20230116'},
+        { name: 'Fiony Alveria', src: 'https://pbs.twimg.com/media/GAUqENmaUAAvvjk?format=jpg&name=medium'},
         { name: 'Flora Shafiq', src: 'https://jkt48.com/profile/flora_shafiq.jpg?v=20230116'},
-        { name: 'Freya Jayawardana', src: 'https://jkt48.com/profile/freya_jayawardana.jpg?v=20230116'},
-        { name: 'Gabriella Abigail', src: 'https://jkt48.com/profile/gabriela_abigail.jpg?v=20230531'},
-        { name: 'Gita Sekar ANdarini', src: 'https://jkt48.com/profile/gita_sekar_andarini.jpg?v=20230116'},
-        { name: 'Helisma Putri', src: 'https://jkt48.com/profile/helisma_putri.jpg?v=20230116'},
-        { name: 'Indah Cahya', src: 'https://jkt48.com/profile/indah_cahya.jpg?v=20230116'},
-        { name: 'Indira Seruni', src: 'https://jkt48.com/profile/indira_seruni.jpg?v=20230531'},
-        { name: 'Jessica Chandra', src: 'https://jkt48.com/profile/jessica_chandra.jpg?v=20230116'},
+        { name: 'Freya Jayawardana', src: 'https://pbs.twimg.com/media/GAU2As8b0AA0QM9?format=jpg&name=medium'},
+        { name: 'Gabriella Abigail', src: 'https://pbs.twimg.com/media/GAUp40daIAAACJe?format=jpg&name=medium'},
+        { name: 'Gita Sekar Andarini', src: 'https://pbs.twimg.com/media/GAVuinubwAA0uZY?format=jpg&name=medium'},
+        { name: 'Helisma Putri', src: 'https://pbs.twimg.com/media/GAUrF4xakAATcHg?format=jpg&name=medium'},
+        { name: 'Indah Cahya', src: 'https://pbs.twimg.com/media/GAU1O0tawAAiULz?format=jpg&name=small'},
+        { name: 'Indira Seruni', src: 'https://pbs.twimg.com/media/GAUp-Arb0AA8ofY?format=jpg&name=medium'},
+        { name: 'Jessica Chandra', src: 'https://pbs.twimg.com/media/GAUqJMrbUAAmb6I?format=jpg&name=medium'},
         { name: 'Jesslyn Elly', src: 'https://jkt48.com/profile/jesslyn_elly.jpg?v=20230531'},
-        { name: 'Kathrina Irene', src: 'https://jkt48.com/profile/kathrina_irene.jpg?v=20230116'},
+        { name: 'Kathrina Irene', src: 'https://pbs.twimg.com/media/GAU1NSUbMAEvPJa?format=jpg&name=medium'},
         { name: 'Lulu Salsabila', src: 'https://jkt48.com/profile/lulu_salsabila.jpg?v=20230116'},
         { name: 'Marsha Lenathea', src: 'https://jkt48.com/profile/marsha_lenathea.jpg?v=20230116'},
         { name: 'Mutiara Azzahra', src: 'https://jkt48.com/profile/mutiara_azzahra.jpg?v=20230116'},
         { name: 'Raisha Syifa', src: 'https://jkt48.com/profile/raisha_syifa.jpg?v=20230530'},
-        { name: 'Reva Fidela', src: 'https://jkt48.com/profile/reva_fidela.jpg?v=20230116'},
-        { name: 'Shani Indira Natio', src: 'https://jkt48.com/profile/shani_indira_natio.jpg?v=20230116'},
-        { name: 'Shania Gracia', src: 'https://jkt48.com/profile/shania_gracia.jpg?v=20230116'},
+        { name: 'Reva Fidela', src: 'https://pbs.twimg.com/media/GAU9swkaUAA9Znp?format=jpg&name=medium'},
+        { name: 'Shani Indira Natio', src: 'https://pbs.twimg.com/media/GAV70MGaYAATPfZ?format=jpg&name=medium'},
+        { name: 'Shania Gracia', src: 'https://pbs.twimg.com/media/GAV4escaYAArTOi?format=jpg&name=medium'},
         { name: 'Yessica Tamara', src: 'https://jkt48.com/profile/yessica_tamara.jpg?v=20230116'},
         ],
         links: ['Home', 'News', 'Member', 'About JKT48', 'Produser', 'Kontak'],
